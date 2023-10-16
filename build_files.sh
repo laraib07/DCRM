7c9adc338ff01208318ce745116f50d94b84d3d5
laraib07 <a.laraib@protonmail.com>
2023-10-16 12:24:48 +0530

    fix: vercel build

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,10 @@
-echo "BUILD START"
-pip install -r requirements.txt
-python3.10 manage.py collectstatic --no-input 
-echo "BUILD FINISHED"
+# Build the project
+echo "Building the project..."
+python3.10 -m pip install -r requirements.txt
+
+echo "Make Migration..."
+python3.10 manage.py makemigrations --noinput
+python3.10 manage.py migrate --noinput
+
+echo "Collect Static..."
+python3.10 manage.py collectstatic --noinput --clear
--- dcrm/settings.py
+++ dcrm/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 from dotenv import load_dotenv
+import dj_database_url
 import os
 
 load_dotenv()
@@ -93,6 +94,10 @@ DATABASES = {
     }
 }
 
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES['default']['OPTIONS']['charset'] = 'utf8mb4'
+del DATABASES['default']['OPTIONS']['sslmode'] 
+DATABASES['default']['OPTIONS']['ssl'] =  {'ca': os.environ.get('MYSQL_ATTR_SSL_CA')}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
