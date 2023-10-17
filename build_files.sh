e10044ad594e397a3a684a225ea74b22e9c8b7aa
laraib07 <a.laraib@protonmail.com>
2023-10-17 11:38:04 +0530

    add: `--no-deps` flag to pip

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 # Build the project
 echo "Building the project..."
-python3.9 -m pip install -r requirements.txt
+python3.9 -m pip install --no-deps -r requirements.txt
 
 echo "Make Migration..."
 python3.9 manage.py makemigrations --noinput
